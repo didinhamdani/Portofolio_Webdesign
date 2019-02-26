d1f83325214eeec52d6a19d6b7fb9044a78b34d5
didinhamdani <nabilap8@gmail.com>
2019-02-26 09:58:36 +0700

    Add Paralax Effect Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,40 @@ $('.page-scroll').on('click', function(e) {
     
     e.preventDefault(); 
 });
+
+// Paralax
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+
+    // Jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    }); 
+
+    //Animasi portofolio
+    if( wScroll > $('.portofolio').offset().top -80){
+      $('.portofolio .thumbnail').each(function(i){
+        setTimeout(function(){
+            $('.portofolio .thumbnail').eq(i).addClass('muncul');  
+        }, 300 * (i+1));
+      });
+      
+        //
+    }
+});
